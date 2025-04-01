cb37d5b8ad0692bf02e7023c7bf36df4a3293877
Kuzminova529 <olakuzminova529@gmail.com>
2025-04-01 16:03:52 +0300

    Spring Boot start

--- domain/src/main/java/org/reservationapplication/domain/spring/DomainConfig.java
+++
@@ -1,9 +0,0 @@
-package org.reservationapplication.domain.spring;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "org.reservationapplication.domain")
-public class DomainConfig {
-}
